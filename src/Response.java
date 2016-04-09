4a1c50ab723d666c4a4e8c9e5ed8548fdff3dd8d
Erin <erinversfeld@gmail.com>
2016-04-09 12:40:31 +0200

    [clean up] Added in comments, removed unused methods

--- src/Response.java
+++ src/Response.java
@@ -75,38 +75,6 @@ public class Response extends Message {
     }
 
     /**
-     * Gets a byte[] representation of the contents of the bodyInput InputStream
-     * Used when sending a Response
-     */
-    private byte[] getBodyInput(){
-        log.log(Level.INFO, "Attempting to get the bodyInput in the form of a byte[]");
-        try{
-            int len = bodyInput.available();
-            byte[] out = new byte[len];
-            try{
-                bodyInput.read(out, 0, len);
-            }
-            catch (IOException e){
-                log.log(Level.SEVERE, "First byte of bodyInput cannot be read. Read documentation.");
-                return null;
-            }
-            catch (NullPointerException e){
-                log.log(Level.SEVERE, "out is null");
-                return null;
-            }
-            catch (IndexOutOfBoundsException e){
-                log.log(Level.SEVERE, "off is "+0+" len is "+len+" b.length is "+out.length);
-                return null;
-            }
-            return out;
-        }
-        catch (IOException e){
-            log.log(Level.SEVERE, "IOException thrown in getBodyInput()");
-        }
-        return null;
-    }
-
-    /**
      * Send the given Response via the given output stream.</br>
      * The method writes the start line, followed by the header
      * fields (one per line), followed by a blank line and then
@@ -117,6 +85,7 @@ public class Response extends Message {
     public static void send(final OutputStream output, final Response response) throws IOException   {
         log.log(Level.INFO, "Attempting to send an HTTP Response to the HTTP GET Request");
         try{
+            //Write the HTTP Response to the OutputStream with the correct formatting
             output.write(response.getStartLine().getBytes());
             output.write("\r\n\r\n".getBytes());
             int line;
